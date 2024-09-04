842098c13f5ccb29bfe1481cc4a23122046c19ec
Dasmitha <ariesanastasia5@gmail.com>
2024-09-04 09:17:15 +0530

    New random messages file created via javascript has been added

---
+++ Messages.js
@@ -0,0 +1,49 @@
+
+const color = ['Red','Blue','Green','Yellow','Purple','Orange','Pink','Black','White','Gray']
+
+let i = Math.floor(Math.random()*10)
+
+if(color[i]==='Red'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Here is the Dad Joke of the day : Why did the tomato turn red? Because it saw the salad dressing!")
+}
+else if (color[i]==='Blue'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Here is the Dad joke of the day : Why was the math book sad? It had too many problems, and it felt a little blue!")
+
+}
+else if (color[i]==='Green'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Why are frogs so happy? Because they eat whatever bugs them!")
+}
+else if (color[i]==='Yellow'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("What do you call a banana that tells jokes? A real peel-y!")
+}
+else if (color[i]==='Purple'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Why did the grape stop in the middle of the road? Because it ran out of juice!")
+}
+else if (color[i]==='Orange'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Why did the orange stop rolling down the hill? It ran out of juice!") 
+}
+else if (color[i]==='Pink'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Why do flamingos stand on one leg? Because if they lifted both, they'd fall over!")
+}
+else if (color[i]==='Black'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Why don’t skeletons fight each other? They don’t have the guts!")
+}
+else if (color[i]==='White'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Why did the scarecrow win an award? Because he was outstanding in his field!")
+}
+else if (color[i]==='Grey'){
+    console.log(`Your color is ${color[i]}`)
+    console.log("Why don’t oysters donate to charity? Because they’re shellfish about their pearls!")
+}
+else {
+    console.log("number not found")
+}
